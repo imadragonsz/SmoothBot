b3907e6aad0669ea35476e51ab11b6bd82d176ab
imadragonsz <mittchelk@gmail.com>
2023-01-06 00:23:11 +0100

    added connection to DB

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,23 +1,24 @@
 const { client } = require("discord.js");
-// const mongoose = require('mongoose');
-// const { Database } = process.env;
+const mongoose = require('mongoose');
+const { Database } = process.env;
 module.exports = {
   name: "ready",
   once: true,
   async execute(client) {
     console.log("The client is now ready!");
 
-    // if (!Database) return;
-    // mongoose
-    // 	.connect(Database, {
-    // 		useNewUrlParser: true,
-    // 		useUnifiedTopology: true
-    // 	})
-    // 	.then(() => {
-    // 		console.log('this client is now connected to the database!');
-    // 	})
-    // 	.catch((err) => {
-    // 		console.log(err);
-    // 	});
+    if (!Database) return;
+    mongoose
+      .set('strictQuery', true)
+    	.connect(Database, {
+    		useNewUrlParser: true,
+    		useUnifiedTopology: true
+    	})
+    	.then(() => {
+    		console.log('this client is now connected to the database!');
+    	})
+    	.catch((err) => {
+    		console.log(err);
+    	});
   },
 };
