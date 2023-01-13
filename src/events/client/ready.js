232b70343edd33612b5696867dd530f62894c159
imadragonsz <mittchelk@gmail.com>
2023-01-13 19:18:25 +0100

    yes

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,5 +1,5 @@
 const { client } = require("discord.js");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Database } = process.env;
 module.exports = {
   name: "ready",
@@ -8,16 +8,16 @@ module.exports = {
     console.log("The client is now ready!");
     if (!Database) return;
     mongoose
-      .set('strictQuery', true)
-    	.connect(Database, {
-    		useNewUrlParser: true,
-    		useUnifiedTopology: true
-    	})
-    	.then(() => {
-    		console.log('this client is now connected to the database!');
-    	})
-    	.catch((err) => {
-    		console.log(err);
-    	});
+      .set("strictQuery", true)
+      .connect(Database, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("this client is now connected to the database!");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
 };
