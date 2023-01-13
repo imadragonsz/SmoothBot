1d88df61a8961e50d46e639a2f69ddd2e1bdb0a6
imadragonsz <mittchelk@gmail.com>
2023-01-13 18:17:44 +0100

    edited the guildMemberAdd event

--- src/events/client/guildMemberAdd.js
+++ src/events/client/guildMemberAdd.js
@@ -22,6 +22,25 @@ module.exports = {
       } catch (error) {
         console.error(error);
       }
+    }
+    if (
+      member.guild.channels.cache.get("942237225167491122") ==
+      "942237225167491122"
+    ) {
+      try {
+        console.log("user joined");
+        const { user, guild } = member;
+        const WelcomeChannel =
+          member.guild.channels.cache.get("942237225167491122");
+        const rules = "895789546497777674";
+        const welcomeMessage = `Welcome <@${member.id}> to the server \b\n Please read the <#${rules}> and enjoy your stay`;
+        const embed = new EmbedBuilder()
+          .setDescription(welcomeMessage)
+          .setColor(0x11111);
+        WelcomeChannel.send({ embeds: [embed] });
+      } catch (error) {
+        console.error(error);
+      }
     } else {
       return;
     }
